e2d1547c904255cb9773346e64696d8953ae9c8e
Apurba Panja <apurbapanja1@gmail.com>
2021-04-21 11:15:55 +0530

    Updated menu-items to naviage with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,27 +12,32 @@ class Directory extends React.Component {
                 {
                     id: 1,
                     title: 'hats',
+                    linkUrl: 'hats',
                     imageUrl: 'https://images.unsplash.com/photo-1613114943550-c2c2628b9ac9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
                 },
                 {
                     id: 2,
                     title: 'jackets',
+                    linkUrl: '',
                     imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
                 },
                 {
                     id: 3,
                     title: 'sneakers',
+                    linkUrl: '',
                     imageUrl: 'https://images.unsplash.com/photo-1612902376491-7a8a99b424e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
                 },
                 {
                     id: 4,
                     title: 'men',
+                    linkUrl: '',
                     imageUrl: 'https://images.unsplash.com/photo-1508243529287-e21914733111?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
                     size: 'large'
                 },
                 {
                     id: 5,
                     title: 'women',
+                    linkUrl: '',
                     imageUrl: 'https://images.unsplash.com/photo-1581404917879-53e19259fdda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80',
                     size:'large'
                 },
@@ -43,8 +48,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({id, title, imageUrl, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );
